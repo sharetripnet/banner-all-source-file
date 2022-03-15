2456c5c26c7312b0ea3de08dee7854a81869bf09
Aman Ullah <amanix007@gmail.com>
2022-03-15 17:55:12 +0600

    initial commit

---
+++ gnr-logo-placement/main.js
@@ -0,0 +1,35 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+<div class="gnr-logo" style="
+    position: absolute;
+    bottom: 0;
+    right: 24px;
+    transform: translate(100%, 0);
+">
+
+    <div class="img" style="
+    margin-right: 8px;
+    display: inline-block;
+    vertical-align: middle;
+">
+  <img src="./favicon.png" alt="gnrads.com" style="
+    width: 18px;
+">
+</div>
+<div class="text" style="
+    display: inline-block;
+    vertical-align: middle;
+    ">
+    <a href="https://gnrads.com/" target="_blank" rel="noopener">gnrads.com</a>
+    </div>
+</div>
+`
+
+
+
+// var img = document.createElement("img");
+// img.src = "./favicon.svg";
+// img.value = "myvalue";
+// select.appendChild(img);
+
